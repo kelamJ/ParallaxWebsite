33058d005032c431fbea9a3c9f065846f0829949
kelamJ <malekjulien80@gmail.com>
2023-11-21 15:13:55 +0100

    Effet parallax complet

--- 3D-Parallax-Website-in-JavaScript/js/app.js
+++ 3D-Parallax-Website-in-JavaScript/js/app.js
@@ -5,12 +5,7 @@ let xValue = 0,
 
 let rotateDegree = 0;
 
-window.addEventListener("mousemove", (e) => {
-    xValue = e.clientX - window.innerWidth / 2;
-    yValue = e.clientY - window.innerHeight / 2;
-
-    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
-
+function update(cursorPosition) {
     parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
@@ -20,7 +15,7 @@ window.addEventListener("mousemove", (e) => {
         let isInLeft =
             parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
         let zValue=
-            (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+            (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
 
         el.style.transform = `perspective(2300px) translateZ(${
             zValue * speedz
@@ -30,4 +25,56 @@ window.addEventListener("mousemove", (e) => {
             yValue * speedy
         }px))`;
     });
+}
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+    if(timeline.isActive()) return;
+
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+    update(e.clientX);
 });
+
+//GSAP Animation
+
+let timeline = gsap.timeline();
+
+Array.from(parallax_el)
+    .filter(el => !el.classList.contains("text"))
+    .forEach((el) => {
+    timeline.from(
+        el,
+        {
+            top: `${el.offsetHeight /2 + +el.dataset.distance}px`,
+            duration: 2.5,
+            ease: "power3.out",
+        },
+        "1"
+    );
+});
+
+timeline.from(".text h1", {
+    y: window.innerHeight -
+        document.querySelector(".text h1").getBoundingClientRect().top + 200,
+    duration: 2,
+        },
+    "2.5"
+).from(
+    ".text h2",
+    {
+    y: -150,
+    opacity: 0,
+    duration: 1.5,
+    },
+    "3"
+)
+    .from(".hide", {
+        opacity: 0,
+        duration: 1.5,
+        }, "3"
+    );
